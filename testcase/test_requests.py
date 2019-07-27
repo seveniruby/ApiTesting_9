00ae58720d1ff82a3021e3ca1f7f43e6a026ae0a
seveniruby <seveniruby@gmail.com>
2019-07-27 12:21:43 +0800

    hamcrest

--- testcase/test_requests.py
+++ testcase/test_requests.py
@@ -2,6 +2,8 @@ import requests
 import logging
 import pytest
 import json
+import jsonpath
+from hamcrest import *
 
 class TestRequests(object):
     logging.basicConfig(level=logging.INFO)
@@ -11,7 +13,6 @@ class TestRequests(object):
         logging.info(r)
         logging.info(r.text)
         logging.info(json.dumps(r.json(), indent=2))
-
 
     def test_post(self):
         r=requests.get(self.url,
@@ -38,7 +39,30 @@ class TestRequests(object):
                      cookies={"u": "3446260779", "xq_a_token":"5806a70c6bc5d5fb2b00978aeb1895532fffe502"}
                      )
         logging.info(json.dumps(r.json(), indent=2))
-        assert r.json()["data"]["category"] == 1
+        assert r.json()["data"]["category"] == 2
+        assert r.json()["data"]["stocks"][0]["name"] == "华宝中短债债券C"
+        assert jsonpath.jsonpath(r.json(),
+                                 "$.data.stocks[?(@.symbol == 'F006947')].name")[0]=="华宝中短债债券A"
+        assert_that(jsonpath.jsonpath(r.json(),"$.data.stocks[?(@.symbol == 'F006947')].name")[0],
+                    equal_to("华宝中短债债券B"), "比较上市代码与名字")
+
+    def test_hamcrest(self):
+        assert_that(0.1 * 0.1, close_to(0.01, 0.000000000000001))
+        #assert_that(0.1 * 0.1, close_to(0.01, 0.000000000000000001))
+        assert_that(
+            ["a", "b", "c"],
+            all_of(
+                has_items("c", "d"),
+                has_items("c", "a")
+            )
+        )
+
+
+
+
+
+
+
 
 
 
